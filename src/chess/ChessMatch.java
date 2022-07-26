13a5be8f5fa31d4e54ee9fa629c43967c5016d84
Tamara Luz <tamarabluz@gmail.com>
2022-07-26 15:19:04 -0300

    Chackmate logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -17,6 +17,7 @@ public class ChessMatch {
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
 	private boolean check;
+	private boolean checkMate;
 	public ChessMatch() {
 		board = new Board(8, 8);
 		turn = 1;
@@ -34,6 +35,10 @@ public class ChessMatch {
 
 	public boolean getCheck(){
 		return check;
+	}
+
+	public boolean getCheckMate(){
+		return checkMate;
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -64,7 +69,12 @@ public class ChessMatch {
 			throw new ChessException("You can't put yourself in check");
 		}
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
-		nextTurn();
+		if(testCheckMate(opponent(currentPlayer))){
+			checkMate = true;
+		}
+		else {
+			nextTurn();
+		}
 		return (ChessPiece)capturedPiece;
 	}
 
@@ -135,6 +145,31 @@ public class ChessMatch {
 			}
 		}
 		return false;
+	}
+
+	private boolean testCheckMate(Color color) throws IllegalAccessException {
+		if(!testCheck(color)){
+			return false;
+		}
+		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == opponent(color)).collect(Collectors.toList());
+		for (Piece p : list){
+			boolean[][] mat = p.possibleMoves();
+			for(int i= 0; i< board.getRows(); i++){
+				for( int j= 0; j< board.getColumns(); j++){
+					if (mat[i][j]){
+						Position source = ((ChessPiece)p).getChessPosition().toPosition();
+						Position target = new Position(i, j);
+						Piece capturePiece = makeMove(source, target);
+						boolean testCheck = testCheck(color);
+						undoMove(source, target, capturePiece);
+						if (!testCheck){
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
 	}
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
